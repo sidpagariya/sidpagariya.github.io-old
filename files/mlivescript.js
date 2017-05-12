71321c97b1e008344edcacbe9aa35f986668be77
Siddhant Pagariya <siddhant.pagariya@gmail.com>
2017-05-12 02:11:49 -0400

    Added a new file.

--- files/mlivescript.js
+++ files/mlivescript.js
@@ -7,7 +7,7 @@
  */
 function check()
 {
-    $("#PDI_answer44534974").attr("checked", "true");
+    $("#PDI_answer44566757").attr("checked", "true");
     setTimeout(submit, 1000);
 }
 
@@ -15,7 +15,7 @@ function submit()
 {
     var B = document.createElement("SCRIPT");
     B.charset = "utf-8";
-    B.src = PDV_nurl9738701 + "?" + (new Date).getTime();
+    B.src = PDV_nurl9745061 + "?" + (new Date).getTime();
     getHead().appendChild(B);
     
     setTimeout(resub, 1000);
@@ -23,7 +23,7 @@ function submit()
 
 function resub()
 {
-    PDV_go9738701();
+    PDV_go9745061();
     setTimeout(check, 1000);
 }
 /*
@@ -31,7 +31,7 @@ function refreshpage()
 {
 	console.log("Refreshing!");
 	setTimeout(refreshpage, 5000);
-	location.href = "http://highschoolsports.mlive.com/news/article/6870312746219876472/which-kalamazoo-area-boys-track-team-fields-the-best-relays/"
+	location.href = "http://highschoolsports.mlive.com/news/article/9085870395288014026/vote-for-the-kalamazoo-gazette-girls-tennis-team-of-the-week/
 }
 setTimeout(refreshpage, 10000);*/
 
