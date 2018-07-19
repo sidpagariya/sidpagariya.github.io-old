953cf04becb16ae6779a6f02229edfbc1be079dd
Siddhant Pagariya <siddhant.pagariya@gmail.com>
2018-07-19 17:05:34 -0400

    Updated files.

--- files/arrays-reenu2.cpp
+++ files/arrays-reenu2.cpp
@@ -145,12 +145,11 @@ void swapRange(char arr1[], int size1, int index1, char arr2[], int size2, int i
 
 void swapWithinOneRow(char arr[], int size, int len)
 {
-
-    if((size>0)&&(size<=SIZE)&&(len>0))
+    for (int i=0; i<size; i+=2*len)
     {
-        if(size%len>=0)
+        if(i+2*len <= size)
         {
-            swapRange(arr,size,0,arr,size,len,len);
+            swapRange(arr,size,i,arr,size,i+len,len);
         }
     }
 }
@@ -161,71 +160,3 @@ void swapRows(char arr[][SIZE], int rows, int cols)
     for(int i=0;i<(rows-1);i+=2)
     swapRange(arr[i],cols,0,arr[i+1],cols,0,cols);
 }
-
-/*
-int main()
-{
-cout << "printRight(3): "<< '\n';
-printRight(3);
-cout << "\nprintRight_upsideDown_rightJustified(3): "<< '\n';
-printRight_upsideDown_rightJustified(3);
-
-const char arr[2][SIZE] = { {'A','3','@'}, {'#','\\','9'} };
-cout << "\nprintArray(arr[0], 3): " << '\n';
-printArray(arr[0], 3);
-cout << "\nprintArray(arr, 2, 3): " << '\n';
-printArray(arr, 2, 3);
-
-char alph[2][SIZE] = {{'~', 'f', 'D', '3', '"', '!', ' '}, {' ', '!', '"', '3', 'D', 'f', '~'}};
-cout << "\nadd(alph[0], 7, 3): " << '\n';
-add(alph[0], 7, 3);
-cout << "\nadd(alph, 2, 7, -3): " << '\n';
-add(alph, 2, 7, -3);
-
-cout << "\nrotateLeft(alph[0], 7): " << '\n';
-rotateLeft(alph[0], 7);
-printArray(alph[0], 7);
-cout << "\nrotateLeft(alph, 2, 7): " << '\n';
-rotateLeft(alph, 2, 7);
-printArray(alph, 2, 7);
-
-cout << "\nrotateRight(alph[0], 7): " << '\n';
-rotateRight(alph[0], 7);
-printArray(alph[0], 7);
-cout << "\nrotateRight(alph, 2, 7): " << '\n';
-rotateRight(alph, 2, 7);
-printArray(alph, 2, 7);
-
-char nums[2][SIZE]={{'1', '2', '3', '4', '5', '6'}, {'7', '8', '9', '0', '1', '2'}};
-cout << "\nswapRange(nums[0], 6, 0, nums[1], 6, 3, 3): " << '\n';
-swapRange(nums[0], 6, 0, nums[1], 6, 3, 3);
-printArray(nums, 2, 6);
-
-char batmanrobin[12] = {'b','a','t','m','a','n','$','r','o','b','i','n'}, br[12];
-memcpy(br, batmanrobin, sizeof(br));
-cout << "\nswapWithinOneRow(batmanrobin, 12, 6): " << '\n';
-swapWithinOneRow(batmanrobin, 12, 6);
-printArray(batmanrobin, 12);
-memcpy(batmanrobin, br, sizeof(batmanrobin));
-cout << "swapWithinOneRow(batmanrobin, 12, 3): " << '\n';
-swapWithinOneRow(batmanrobin, 12, 3);
-printArray(batmanrobin, 12);
-memcpy(batmanrobin, br, sizeof(batmanrobin));
-cout << "swapWithinOneRow(batmanrobin, 12, 5): " << '\n';
-swapWithinOneRow(batmanrobin, 12, 5);
-printArray(batmanrobin, 12);
-memcpy(batmanrobin, br, sizeof(batmanrobin));
-cout << "swapWithinOneRow(batmanrobin, 12, 8): " << '\n';
-swapWithinOneRow(batmanrobin, 12, 8);
-printArray(batmanrobin, 12);
-
-char abc[4][SIZE] = {{'a', 'b', 'c'}, {'d', 'e', 'f'}, {'g', 'h', 'i'}, {'j', 'k', 'l'}};
-cout << "\nswapRows(abc, 3, 3): " << '\n';
-swapRows(abc, 4, 3);
-printArray(abc, 4, 3);
-
-char arr2[5]={'a', 'b', 'c', '0'};
-add(arr2, 5, -953);
-printArray(arr2, 5);
-}
-*/
